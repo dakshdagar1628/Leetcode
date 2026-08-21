8bd2afbf72209719fc3d85b7088c73aea64034be
daksh <dakshdagar1628@gmail.com>
2026-08-21 16:22:07 +0530

    Time: 123 ms (21.30%) | Memory: 131.4 MB (10.36%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
@@ -3,15 +3,18 @@ public:
     long long beautifulSubarrays(vector<int>& nums) {
         long long xr = 0;
         long long cnt = 0;
-        map<long long,long long> mpp;
-        mpp[xr]++;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            xr ^= nums[i];
-            long long x = xr ^ 0;
-            cnt += mpp[x]; 
+
+        unordered_map<long long, long long> mpp;
+        mpp[0] = 1;
+
+        for (int num : nums) {
+            xr ^= num;
+
+            cnt += mpp[xr];
+
             mpp[xr]++;
         }
+
         return cnt;
     }
 };
