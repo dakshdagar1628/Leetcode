b1436623703a40313778ec1acff5519fc0281fca
daksh <dakshdagar1628@gmail.com>
2026-09-05 21:50:23 +0530

    Time: 3 ms (67.83%) | Memory: 15.7 MB (50.06%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(vector<string> &v, int oc, int cc, int n,string s )
+    {
+        if(oc == n && cc == n){
+        v.push_back(s);
+        return ;}
+        if(oc < n )
+        helper(v,oc + 1,cc,n,s+"(");
+        if(cc < oc )
+        helper(v,oc,cc + 1,n,s+")");
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        int cc = 0, oc =0;
+        helper(v, oc, cc, n, "");
+        return v;
+    }
+};
