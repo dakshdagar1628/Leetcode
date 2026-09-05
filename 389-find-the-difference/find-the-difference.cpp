f6fe2533930b31867c0ac9cdbf7ab8d6331773c1
daksh <dakshdagar1628@gmail.com>
2026-09-05 22:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (25.62%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int Xor = 0;
+        for(char &ch: t)
+        {
+            Xor ^= ch;
+        }
+        for(char &ch: s)
+        {
+            Xor ^= ch;
+        }
+        return (char)Xor;
+    }
+};
